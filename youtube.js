208e31f6e2ec5242d44deb7d6fef225faf03743a
Olumide Sajowa <olumidesajowa@gmail.com>
2024-12-01 09:48:26 +0100

    Third Save

---
+++ youtube.js
@@ -0,0 +1,33 @@
+
+let age = 35
+let street = ` No 24 Ikoyi `
+let isCustomer = true 
+
+console.log(age)
+console.log(street)
+console.log(isCustomer)
+
+let outcome = 100 - 50
+console.log(outcome)
+
+let result = 100 + 100
+console.log(result)
+
+let over = 10 * 10
+console.log(over)
+
+let div = 500 / 50
+console.log(div)
+
+let modulusResult = 11%3
+console.log(modulusResult)
+
+let powerResult = 2 ** 4
+console.log(powerResult)
+
+let firstItemPrice = 100
+let secondItemPrice = 20
+let thirdItemPrice = 500
+let totalPrice = firstItemPrice + secondItemPrice + thirdItemPrice
+console.log(totalPrice)
+
